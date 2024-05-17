5c8db64346988bbb7b54580c4a784dfd89ef1b49
Ali Abrorov <ahrorabrorov26@gmail.com>
2024-05-17 08:08:51 +0500

    fifth commit

--- src/auth/Auth0ProviderWithNavigate.tsx
+++ src/auth/Auth0ProviderWithNavigate.tsx
@@ -12,7 +12,7 @@ export default function Auth0ProviderWithNavigate({ children }: Props) {
   const domain = import.meta.env.VITE_AUTH0_DOMAIN;
   const clientId = import.meta.env.VITE_AUTH0_CLIENT_ID;
   const redirectUri =
-    import.meta.env.VITE_AUTH0_CALLBACK_URL || "http://localhost:5173";
+    import.meta.env.VITE_AUTH0_CALLBACK_URL 
   const audience = import.meta.env.VITE_AUTH0_AUDIENCE;
 
   if (!domain || !clientId || !redirectUri || !audience) {
